38d950ce55f1b471cb8f4f69addd40b0d59ce336
Maryam Ahmed <msoliman@stingrayconsultation.com>
2025-08-19 22:31:34 +0300

    implement priority configuration

---
+++ TaskyRevamp.Domain/Models/SystemConfiguration/priorityConfiguartion.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskyRevamp.Dto.SystemConfiguration;
+
+namespace TaskyRevamp.Domain.Models.SystemConfiguration
+{
+	public class priorityConfiguartion : Entity
+	{
+		public string NameEnglish { get; set; }
+		public string NameArabic { get; set; }
+		public string NameColor { get; set; }
+		public string BackgroundColor { get;  set; }
+		public int Ranking { get; set; }
+		public int Order { get; set; }
+
+		public priorityConfiguartion()
+		{
+
+		}
+
+		public priorityConfiguartion(string nameEnglish, string nameArabic, string nameColor, string backgroundColor, int ranking, int order)
+		{
+			NameEnglish = nameEnglish;
+			NameArabic = nameArabic;
+			NameColor = nameColor;
+			BackgroundColor = backgroundColor;
+			Ranking = ranking;
+			Order = order;
+		}
+
+		//public void update()
+		//{
+
+		//}
+		 public PriorityDto CopyToDto()
+		{
+			return new PriorityDto
+			{
+				Id = Id,
+				NameEnglish = NameEnglish,
+				NameArabic = NameArabic,
+				NameColor = NameColor,
+				BackgroundColor = BackgroundColor,
+				Ranking = Ranking,
+				Order = Order
+			};
+		}
+	}
+}
---
+++ TaskyRevamp.Dto/SystemConfiguration/PriorityDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskyRevamp.Dto.SystemConfiguration
+{
+	public class PriorityDto
+	{
+		public Guid Id { get; set; }
+		public string NameEnglish { get; set; }
+		public string NameArabic { get; set; }
+		public string NameColor { get; set; }
+		public string BackgroundColor { get; set; }
+		public int Ranking { get; set; }
+		public int Order { get; set; }
+	}
+}
