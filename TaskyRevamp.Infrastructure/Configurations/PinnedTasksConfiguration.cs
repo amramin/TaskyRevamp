dea6e6d1b6aae2d2b8de3b11b0e3eec667f334f1
wafaa.abdou <wabdou@stingrayconsultation.com>
2025-09-07 17:06:35 +0300

    pinned

---
+++ TaskyRevamp.Infrastructure/Configurations/PinnedTasksConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using TaskyRevamp.Domain.Models.Task;
+
+namespace TaskyRevamp.Infrastructure.Configurations;
+
+internal class PinnedTasksConfiguration : IEntityTypeConfiguration<PinnedTasks>
+{
+    public void Configure(EntityTypeBuilder<PinnedTasks> builder)
+    {
+        builder
+   .HasOne(te => te.Task)
+   .WithMany().HasForeignKey(k=>k.TaskId)
+   .OnDelete(DeleteBehavior.Restrict); // or NoAction in EF Core 5+
+
+     
+
+    }
+}
---
+++ TaskyRevamp.Services/PinnedTasks/Command/DeleteDepartmentCommand.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+using TaskyRevamp.Domain.Models.Task;
+using TaskyRevamp.Domain.Repositeries;
+
+namespace TaskyRevamp.Services.PinnedTaskss.Commands;
+
+public record DeletePinnedTasksCommand(Guid Id) : IRequest<bool>;
+
+public class DeleteGroupCommandHandler : IRequestHandler<DeletePinnedTasksCommand, bool>
+{
+    private readonly IRepository<PinnedTasks> _tskRepository;
+
+    public DeleteGroupCommandHandler(IRepository<PinnedTasks> tskRepository)
+    {
+        _tskRepository = tskRepository;
+    }
+
+    public async Task<bool> Handle(DeletePinnedTasksCommand request, CancellationToken cancellationToken)
+    {
+      
+      
+
+            await _tskRepository.Delete(request.Id);
+
+      
+        return true;
+    }
+}
