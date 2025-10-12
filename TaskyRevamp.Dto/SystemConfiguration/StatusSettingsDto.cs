e71783c7a24e5944f55cc50eb2767f01975b49ee
Maryam Ahmed <msoliman@stingrayconsultation.com>
2025-10-12 23:10:33 +0300

    update v2

--- TaskyRevamp.Dto/SystemConfiguration/StatusSettingsDto.cs
+++ TaskyRevamp.Dto/SystemConfiguration/StatusSettingsDto.cs
@@ -1,17 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TaskyRevamp.Localization.Resources;
 
 namespace TaskyRevamp.Dto.SystemConfiguration
 {
 	public class StatusSettingsDto
 	{
 		public Guid Id { get; set; }
+		[Required(ErrorMessageResourceType = typeof(SharedResources), ErrorMessageResourceName = "Required")]
 		public string NameEnglish { get; set; }
+		[Required(ErrorMessageResourceType = typeof(SharedResources), ErrorMessageResourceName = "Required")]
 		public string NameArabic { get; set; }
+		[Required(ErrorMessageResourceType = typeof(SharedResources), ErrorMessageResourceName = "Required")]
 		public string NameColor { get; set; }
+		[Required(ErrorMessageResourceType = typeof(SharedResources), ErrorMessageResourceName = "Required")]
 		public string BackgroundColor { get; set; }
 	}
 }
