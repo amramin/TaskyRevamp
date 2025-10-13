844f629bc5a32678ebe9797affae42a468772034
Ayman <asabra@stingrayconsultation.com>
2025-10-13 20:56:43 +0300

    Sortable Table

---
+++ TaskyRevamp/TaskyRevamp.Client/Shared/SortableColumn.cs
@@ -0,0 +1,8 @@
+﻿namespace TaskyRevamp.Client.Shared;
+
+public class SortableColumn
+{
+    public string Field { get; set; } = string.Empty;
+    public string DisplayName { get; set; } = string.Empty;
+    public bool IsSortable { get; set; } = true; // Default: sortable
+}
