6c01f7275bfd75c7aa2e59d46d171dc9d3b7d278
wafaa.abdou <wabdou@stingrayconsultation.com>
2025-10-15 11:23:57 +0300

    sortable table, search ,evennts, update,grid,popus

---
+++ TaskyRevamp.Dto/Enums/SearchFields/SearchFieldDepartment.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskyRevamp.Dto.Enums.SearchFields
+{
+
+    public enum SearchFieldDepartment
+    {
+        NameEnglish,
+        NameArabic,
+        ParentNameEN,
+        ParentNameAR,
+
+        CreateDate,
+        CreatedBy,
+        UpdateDate,
+        UpdatedBy,
+        
+    }
+}
