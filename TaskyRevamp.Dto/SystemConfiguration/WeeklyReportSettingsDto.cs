bb16c42bbdf236e50db932a3c76879e4e2ef7285
Maryam Ahmed <msoliman@stingrayconsultation.com>
2025-11-09 22:10:40 +0200

    fix weekly report

--- TaskyRevamp.Dto/SystemConfiguration/WeeklyReportSettingsDto.cs
+++ TaskyRevamp.Dto/SystemConfiguration/WeeklyReportSettingsDto.cs
@@ -1,11 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TaskyRevamp.Dto.Enums;
-using TaskyRevamp.Localization.Resources;
 
 namespace TaskyRevamp.Dto.SystemConfiguration
 {
