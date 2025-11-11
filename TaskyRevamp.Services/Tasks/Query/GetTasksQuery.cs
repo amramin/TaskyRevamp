2c08c7861d3ec21a3cedf26f8db61b51471c20d2
wafaa.abdou <wabdou@stingrayconsultation.com>
2025-11-11 21:32:42 +0200

    dn

--- TaskyRevamp.Services/Tasks/Query/GetTasksQuery.cs
+++ TaskyRevamp.Services/Tasks/Query/GetTasksQuery.cs
@@ -73,7 +73,7 @@ public class GetTasksHandler : IRequestHandler<GetTasksQuery, PagedResult<Create
             tasky.TaskStatusName = currentCulture == "ar" ? tsk.status?.NameArabic : tsk.status?.NameEnglish;
             tasky.CreatedByName = currentCulture == "ar" ? tsk.CreatedBy?.NameArabic : tsk.CreatedBy?.NameEnglish;
             tasky.UpdatedBy = currentCulture == "ar" ? tsk.UpdatedBy?.NameArabic : tsk.UpdatedBy?.NameEnglish;
-            tasky.AssigneduserNames = string.Join(",", assgnedusr.Value.Select(k => k.Username));// string.Join(", ", tsk.Assignees.Select(k => k.User.NameEnglish));
+            tasky.AssigneduserNames = string.Join(",", assgnedusr.Value.Select(k => k.NameEnglish));// string.Join(", ", tsk.Assignees.Select(k => k.User.NameEnglish));
             if (tasky.AssigneduserNames.Count() > 0)
             {
                 var initials = string.Join(", ",
