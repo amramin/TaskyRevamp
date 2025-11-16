0f9be774e926d55f7aa73f85a03e98fbb6a9c5c0
wafaa.abdou <wabdou@stingrayconsultation.com>
2025-11-16 22:15:10 +0200

    will

---
+++ TaskyRevamp.Dto/Enums/SearchFields/SearchFieldUserDepartment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskyRevamp.Dto.Enums.SearchFields
+{
+
+    public enum SearchFieldUserDepartment
+    {
+        Name,
+        Emial,
+        Privilage,
+
+
+    }
+}
