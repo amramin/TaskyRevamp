733c76a390b40ae379595b5e6f872426f41cf275
Maryam Ahmed <msoliman@stingrayconsultation.com>
2025-12-10 15:40:48 +0200

    update naming for privilege page hader

--- TaskyRevamp.Localization/Resources/SharedResources.Designer.cs
+++ TaskyRevamp.Localization/Resources/SharedResources.Designer.cs
@@ -1915,15 +1915,6 @@ namespace TaskyRevamp.Localization.Resources {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Privilege .
-        /// </summary>
-        public static string Privilege {
-            get {
-                return ResourceManager.GetString("Privilege", resourceCulture);
-            }
-        }
-        
-        /// <summary>
         ///   Looks up a localized string similar to You can&apos;t duplicate the privilege name.
         /// </summary>
         public static string PrivilegeDuplicateValidation {
