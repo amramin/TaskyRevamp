3a779f47053f8d52ed98379ea25a12e60251e89f
wafaa.abdou <wabdou@stingrayconsultation.com>
2025-12-16 12:17:16 +0200

    willcompelete

---
+++ TaskyRevamp.Dto/Enums/AuthenticationMode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskyRevamp.Dto.Enums
+{
+    public enum AuthenticationMode
+    {
+        Database = 0,
+        ActiveDirectory = 1
+    }
+}
