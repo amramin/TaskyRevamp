12b6ae5e9139d45c5c04ff98eb35188fc3aba10b
taboumadian <taboumadian@stingrayconsultation.com>
2025-12-28 18:22:41 +0200

    edit

--- TaskyRevamp.Services/Tasks/Commands/CreateTaskCommand.cs
+++ TaskyRevamp.Services/Tasks/Commands/CreateTaskCommand.cs
@@ -64,19 +64,19 @@ public class CreateTaskHandler : IRequestHandler<CreateTaskCommand, string>
         {
             if (request.CreateTaskDto.ActualProcess == 0 && (request.CreateTaskDto.StartDate > DateTime.Now))
             {
-                task.StatusId = TaskSatuses.Value.FirstOrDefault(s => s.NameEnglish == "Not started").Id;
+                task.StatusId = Guid.Parse("547022EA-EF8C-4FBC-2236-08DE3318A61C");
             }
             else if (request.CreateTaskDto.ActualProcess == 0 && (request.CreateTaskDto.StartDate <= DateTime.Now))
             {
-                task.StatusId = TaskSatuses.Value.FirstOrDefault(s => s.NameEnglish == "To do").Id;
+                task.StatusId = Guid.Parse("9843AF9D-1389-4740-B428-08DE3D8A77AB");
             }
             else if (request.CreateTaskDto.ActualProcess > 0 && request.CreateTaskDto.ActualProcess < 100)
             {
-                task.StatusId = TaskSatuses.Value.FirstOrDefault(s => s.NameEnglish == "In progress").Id;
+                task.StatusId = Guid.Parse("753404A6-8B18-43F7-2238-08DE3318A61C");
             }
             else if (request.CreateTaskDto.ActualProcess == 100)
             {
-                task.StatusId = TaskSatuses.Value.FirstOrDefault(s => s.NameEnglish == "Pending review").Id;
+                task.StatusId = Guid.Parse("6EE4574D-C439-45B4-223A-08DE3318A61C");
             }
         }
         await _taskRepository.Insert(task);
