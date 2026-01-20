02e9240517abf2dfadc93064700a412b10de4b7a
Maryam Ahmed <msoliman@stingrayconsultation.com>
2026-01-20 20:28:55 +0200

    fix bugs

---
+++ TaskyRevamp.Services/Tasks/Query/GetMainAndParentTasksQuery.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskyRevamp.Domain.Interfaces.Repositeries;
+using TaskyRevamp.Domain.Models.Task;
+using TaskyRevamp.Domain.Repositeries;
+using TaskyRevamp.Dto.TaskDto;
+
+namespace TaskyRevamp.Services.Tasks.Query
+{
+	public record GetMainAndParentTasksQuery() : IRequest<List<CreateTaskDto>>;
+	public class GetMainAndParentTasksHandler : IRequestHandler<GetMainAndParentTasksQuery, List<CreateTaskDto>>
+	{
+		private readonly IRepository<TaskItem> _taskRepository;
+		public GetMainAndParentTasksHandler(IRepository<TaskItem> taskRepository) => _taskRepository = taskRepository;
+		public async Task<List<CreateTaskDto>> Handle(GetMainAndParentTasksQuery request, CancellationToken cancellationToken)
+		{
+			List<CreateTaskDto> taskDtos = new List<CreateTaskDto>();
+			var res = await _taskRepository.FindBy(t => (t.Parent == null && !t.Subtasks.Any()) || t.Subtasks.Any());
+			if (res.Success && res.Value != null && res.Value.Any())
+			{
+				var tasks = res.Value;
+				taskDtos = tasks.Select(t => t.CopyToDto()).ToList();	
+			}
+			return taskDtos;
+		}
+	}
+}
