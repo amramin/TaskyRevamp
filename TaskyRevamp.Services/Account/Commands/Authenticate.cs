0b99c66f29f708cf019c431790a0f349001935d8
taboumadian <taboumadian@stingrayconsultation.com>
2026-03-31 21:03:33 +0200

    edit

--- TaskyRevamp.Services/Account/Commands/Authenticate.cs
+++ TaskyRevamp.Services/Account/Commands/Authenticate.cs
@@ -87,11 +87,6 @@ public class AuthenticateCommandHandler : IRequestHandler<AuthenticateCommand, s
                 }
 
             }
-            else
-            {
-                var userResponse = await _userRepository.FindBy(x => x.Username == request.Username);
-                user = userResponse.Value.FirstOrDefault()!;
-            }
 
 
             var tokenHandler = new JwtSecurityTokenHandler();
